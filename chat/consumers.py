9b6169ed55a7eeae26a244b13065aeccc8c1f910
Salman Mammadli <salmanmammadli@hotmail.com>
2025-08-31 14:45:30 +0400

    typing indicator

--- chat/consumers.py
+++ chat/consumers.py
@@ -1,41 +1,26 @@
 import json
-from chat.models import ChatGroup,GroupMessages
-from channels.generic.websocket import WebsocketConsumer
-from django.shortcuts import get_object_or_404
-from asgiref.sync import async_to_sync
-from django.template.loader import render_to_string
-
-import json
-from channels.generic.websocket import WebsocketConsumer
-from asgiref.sync import async_to_sync
-from django.shortcuts import get_object_or_404
 from chat.models import ChatGroup, GroupMessages
+from channels.generic.websocket import WebsocketConsumer
+from django.shortcuts import get_object_or_404
+from asgiref.sync import async_to_sync
 
 class ChatRoomConsumer(WebsocketConsumer):
-    ## when user connects to the chat
     def connect(self):
         self.user = self.scope['user']
         self.chatroom_name = self.scope['url_route']['kwargs']['chatroom_name']
         self.chatroom = get_object_or_404(ChatGroup, chat_name=self.chatroom_name)
-        # print(self.scope['url_route'])
-        #results: {'args': (), 'kwargs': {'chatroom_name': 'new-chat'}}
-        # print(self.scope['url_route']['kwargs'])
-        #results: {'chatroom_name': 'new-chat'}
 
         async_to_sync(self.channel_layer.group_add)(
             self.chatroom_name,
             self.channel_name
         )
-        # print(f"CHANNEL LAYER: {self.channel_layer}")
-        #RESULTS: CHANNEL LAYER: RedisChannelLayer(hosts=[{'host': '127.0.0.1', 'port': 6379}])
+
         if self.user not in self.chatroom.online_members.all():
             self.chatroom.online_members.add(self.user)
 
         self.accept()
 
     def disconnect(self, close_code):
-        ## when user disconnects from the chat
-
         async_to_sync(self.channel_layer.group_discard)(
             self.chatroom_name,
             self.channel_name
@@ -43,39 +28,51 @@ class ChatRoomConsumer(WebsocketConsumer):
         if self.user in self.chatroom.online_members.all():
             self.chatroom.online_members.remove(self.user)
 
-
     def receive(self, text_data):
-        #text_data is a JSON string sent by the client via a Websocket
         data = json.loads(text_data)
-        content = data['content'].strip()
-        if not content:
-            return
-        ## we take the value and create it
-        message = GroupMessages.objects.create(
-            content=content,
-            author=self.user,
-            group=self.chatroom,
-        )
-        ##then we send it to event to use in message handler
-        event = {
-            "type": "message_handler",
-            "message_id": message.id,
-        }
-        # Send the event to the channel layer group 
-        async_to_sync(self.channel_layer.group_send)(
-            self.chatroom_name,
-            event
-        )
+
+        if 'content' in data:  # message
+            content = data['content'].strip()
+            if content:
+                message = GroupMessages.objects.create(
+                    content=content,
+                    author=self.user,
+                    group=self.chatroom,
+                )
+                event = {
+                    "type": "message_handler",
+                    "message_id": message.id,
+                }
+                async_to_sync(self.channel_layer.group_send)(
+                    self.chatroom_name,
+                    event
+                )
+
+        elif 'typing' in data:  # typing
+            typing_status = data['typing']
+            async_to_sync(self.channel_layer.group_send)(
+                self.chatroom_name,
+                {
+                    "type": "typing_status",
+                    "typing": typing_status,
+                    "user": self.user.username,   
+                }
+            )
+
+    # def message_handler(self, event):
+    #     message_id = event['message_id']
+    #     message = GroupMessages.objects.get(id=message_id)
+    #     self.send(text_data=json.dumps({
+    #         "action": "new_message",
+    #         "author": message.author.username,
+    #         "content": message.content,
+    #         "timestamp": str(message.created_at),
+    #     }))
 
 
-    def message_handler(self, event):
-        ## when a message is received we take the message
-        message_id = event['message_id']
-        message = GroupMessages.objects.get(id=message_id)
-        ## send the message to the WebSocket to all connected clients
-        # now we format the message and send it to the WebSocket to that chatroom
+    def typing_status(self, event):
         self.send(text_data=json.dumps({
-            "author": message.author.username,
-            "content": message.content,
-            "timestamp": str(message.created_at),
+            "action": "typing",
+            "typing": event['typing'],
+            "user": event['user'],   # 👈 send user too
         }))
